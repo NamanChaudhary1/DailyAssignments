d704b7881aeae74f4a8a2a72f747e37c2f1f5743
Naman Chaudhary <91721649+NamanChaudhary1@users.noreply.github.com>
2023-06-08 17:25:47 +0530

    Update project1.py

--- 6 June/Mini Project 1/project1.py
+++ 6 June/Mini Project 1/project1.py
@@ -3,6 +3,7 @@ Set         = set([-12, -3, -6, 7, 2, -2, 6, 3, 9, -7, -5, -8, 1, 11, -9, -4])
 SetSize     = 5
 ResultList  = set()    
 Iterations  = 1000   
+r.seed(42)
 for i in range(Iterations):
     Chromosome = r.sample(Set,SetSize)
     Chromosome.sort()
